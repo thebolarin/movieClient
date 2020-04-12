12ed7a5c6c2940e99afab18945a3937013b85032
thebolarin <odutusinmoses@gmail.com>
2020-04-13 00:30:06 +0100

    dist file added

---
+++ dist/precache-manifest.e63201ab18c6ca804fa9a4161c9a7caa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3b34f7bda289d3969f3e7490dad37e",
+    "url": "/css/Flaticon.eot"
+  },
+  {
+    "revision": "ec808ae82900ae7521f4e3ac79b142d5",
+    "url": "/css/Flaticon.svg"
+  },
+  {
+    "revision": "2b705c306bf93dafc66defed03cf69f6",
+    "url": "/css/Flaticon.ttf"
+  },
+  {
+    "revision": "d3346750587e93e1bf3dac998f2a8435",
+    "url": "/css/Flaticon.woff"
+  },
+  {
+    "revision": "657222ed6394deef6c4c259746641465",
+    "url": "/css/Flaticon.woff2"
+  },
+  {
+    "revision": "631b966ee5a3d92af867382dd71dc544",
+    "url": "/css/_flaticon.scss"
+  },
+  {
+    "revision": "2b7564f4e66f3b3eef05845f1d28afd1",
+    "url": "/css/flaticon.css"
+  },
+  {
+    "revision": "626a05780fee72d420297d8fff544eac",
+    "url": "/css/materialize.css"
+  },
+  {
+    "revision": "ec1df3ba49973dcb9ff212f052d39483",
+    "url": "/css/materialize.min.css"
+  },
+  {
+    "revision": "e242860d3611a7f1327ba3c188edf1c2",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "688d0b93325319a8135996adbd5b69b5",
+    "url": "/img/pro.png"
+  },
+  {
+    "revision": "9326ad44ae4bebdedd141e7a53c2a730",
+    "url": "/img/sort_asc.png"
+  },
+  {
+    "revision": "9a6486086d09bb38cf66a57cc559ade3",
+    "url": "/img/sort_both.png"
+  },
+  {
+    "revision": "1fc418e33fd5a687290258b23fac4e98",
+    "url": "/img/sort_desc.png"
+  },
+  {
+    "revision": "7debd91bae005edafaec8ee65ab0d6ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54553607cd7abe914d19",
+    "url": "/js/app.fb4f217e.js"
+  },
+  {
+    "revision": "b9147c582816cfb202a2",
+    "url": "/js/chunk-vendors.dddd7c6a.js"
+  },
+  {
+    "revision": "9832259e6e013b2e55f342c053c26104",
+    "url": "/js/materialize.js"
+  },
+  {
+    "revision": "5dcfc8944ed380b2215dc28b3f13835f",
+    "url": "/js/materialize.min.js"
+  },
+  {
+    "revision": "922a3e82d06b477d07696203e62bbf94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
